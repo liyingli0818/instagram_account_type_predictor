4159d193ce5abd187b0ef6b53d7edf6547ef4935
Liying Li <liyingl2@illinois.edu>
2018-11-28 12:14:28 -0800

    Modified model score evaluation method

--- src/model.py
+++ src/model.py
@@ -4,6 +4,7 @@ from scipy import stats
 import matplotlib.pyplot as plt
 from scipy.special import comb
 
+from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeRegressor, DecisionTreeClassifier
 from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
 from sklearn.metrics import log_loss, make_scorer
@@ -11,29 +12,32 @@ from sklearn.model_selection import cross_val_score, train_test_split, GridSearc
 
 
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2, random_state=0)
+
+# train test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=4)
+    
+
+# logistic regression
+def fit(X_train, y_train):
+    model = LogisticRegression()
+    model.fit(X_train, y_train)
+    return model
+
+def predict_proba(X_test, X, y, model):
+    y_pred = model.predict_proba(X_test)
+    cv_logloss = -cross_val_score(model, X, y, scoring = 'neg_log_loss', cv = 10).mean()
+    return y_pred, cv_logloss
 
 
-model = DecisionTreeClassifier()
+# random forest
+def fit(X_train, y_train):
+    rf = RandomForestClassifier(n_estimators=100,
+                            n_jobs=-1,
+                            random_state=1)
+    rf.fit(X_train, y_train)
+   
+def predict_proba(X_test, X, y, model):
+    y_pred = rf.predict_proba(X_test)[:, 1]
+    cv_logloss_rf1 = -cross_val_score(rf, X_train, y_train, cv = 10, scoring = 'neg_log_loss').mean()
+    return y_pred, cv_logloss_rf1
 
-depth_parm = np.arange(1, 12, 1)
-num_samples_parm = np.arange(5,95,10)
-parameters = {'max_depth' : depth_parm,
-             'min_samples_leaf' : num_samples_parm}
-
-scorer = make_scorer(log_loss,
-                     greater_is_better=False,
-                     needs_proba=True)
-clf = GridSearchCV(model,
-                   parameters,
-                   cv=10,
-                   scoring=scorer)
-clf.fit(X_train,y_train)
-
-rf = RandomForestClassifier(n_estimators=1000,
-                           max_features='auto',
-                           random_state=0)
-rf.fit(X_train, y_train)
-                                     
-# Test Prediction
-pred = rf.predict(X_test)
