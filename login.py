8a2e5722710178b2cdd8374b450032b9d7d28855
Liying Li <liyingl2@illinois.edu>
2018-12-03 11:10:54 -0800

    Wrap methods up to generate automated login in login.py file

---
+++ login.py
@@ -0,0 +1,33 @@
+from urllib.request import urlopen as uReq
+import pandas as pd
+import requests
+import json
+import yaml
+import time
+import random
+import bs4
+from bs4 import BeautifulSoup 
+from selenium.webdriver import Chrome, Firefox
+
+
+browser = Chrome()
+
+
+
+def load_login_info(filename='/Users/liliying/.secrets/instagram.yaml'):
+    with open(filename) as f:
+        login_info = yaml.load(f)
+    username = login_info['username']
+    password = login_info['password']
+    return username, password
+
+def site_login(url):
+    browser.get (url)
+    browser.find_element_by_css_selector("a.tdiEy").click()
+    username, password = load_login_info()
+    time.sleep(2)
+    browser.find_element_by_name('username').send_keys(username)
+    browser.find_element_by_name('password').send_keys(password)
+    browser.find_element_by_class_name('bkEs3').click()
+
+
