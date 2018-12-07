ee20c20f528ad55f584d000c8a2da99fd50b0ab6
Liying Li <liyingl2@illinois.edu>
2018-12-07 14:36:00 -0800

    Add app.py and index.html that will give the prediction result

--- app.py
+++ app.py
@@ -3,20 +3,29 @@ from src.DataPreparation import get_one_user_df, get_pred_one
 import pickle
 
 
-
-#loaded_model = pickle.load(open('finalized_model.sav', 'rb'))
+#url = instagram.com/username
+loaded_model = pickle.load(open('finalized_model.sav', 'rb'))
 #prediction = get_pred_one(url, loaded_model)
 
 
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
+
+
 
 @app.route("/")
 def Homepage():
-    return render_template('index.html')
+    return render_template('index.html', pred =20)
+
+@app.route('/predict_account_type/<username>', methods=['GET'])
+def predict_account_type(username):
+    url = 'http://instagram.com/%s' % username
+    prediction = str(get_pred_one(url, loaded_model)[0])
+    return prediction
+
 
 
 if __name__ == '__main__':
-    app.run(debug=True, pred = 20)
+    app.run(debug=True)
 
